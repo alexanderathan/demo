794c7ce57d88ae7cb12bdf4940f249a145a8edc7
Alexandros Athanasiadis <alexanderathan@gmail.com>
2020-08-28 22:04:17 +0300

    Created tab bar project with iOS 10.0 restriction

---
+++ Demo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Demo
+//
+//  Created by Alexandros Athanasiadis on 28/8/20.
+//  Copyright © 2020 Alexandros Athanasiadis. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
